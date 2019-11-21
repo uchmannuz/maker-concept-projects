f73a77fcc93fc5d8292ac9450bd25463d2223550
Uchenna Innocent Nwanyanwu <uchenna.nwanyanwu@yahoo.com>
2019-11-21 08:59:32 -0600

    1st update

--- sentence-summary.cpp
+++ sentence-summary.cpp
@@ -12,4 +12,57 @@
 
 using namespace std;
 
+void summary(string input)
+{
+	if(input.size() == 0)
+	{
+		cout << "The supplied String is empty. Please supply a valid String" << endl;
+	}else
+	{
+		//Ascii has 256 characters
+		int characterSummary[256];
+		//Get array size
+		int size = sizeof(characterSummary) / sizeof(characterSummary[0]);
+		//Initialize characterSummary array
+		for(int i = 0; i < size; i++)
+		{
+			characterSummary[i] = 0;
+		}
+		int wordCount = 0;
+		bool foundCharacter = false;
+		//Get character count
+		for (auto x : input)
+		{
+			if(x == ' ' || x == '.' || x == ',' || x == '!')
+			{
+				if(!foundCharacter)
+				{
+					++wordCount;
+				}
+				foundCharacter = true;
+			}else
+			{
+				foundCharacter = false;
+			}
+			++characterSummary[(int)x];
+		}
+		if(!foundCharacter)
+		{
+			++wordCount;
+		}
+		cout << "Total Number of Words = " << wordCount << endl;
+		for(int i = 0; i < size; i++)
+		{
+			if(characterSummary[i] > 0)
+			{
+				char a = i;
+				cout << a << " = " << characterSummary[i] << endl;
+			}
+		}
+	}
+}
 
+int main() {
+	summary("The Courses can be, entered in. any order but the double commas.");
+	return 0;
+}
