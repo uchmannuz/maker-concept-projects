19f3fcca467cf3e85b6efc009d9473ed97f8b3ab
Uchenna Innocent Nwanyanwu <uchenna.nwanyanwu@yahoo.com>
2019-11-21 00:44:09 -0600

    Initial Commit

---
+++ sentence-summary.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : sentence-summary.cpp
+// Author      : Uchenna Nwanyanwu
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+
---
+++ student-grading-system.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : student-grading-system.cpp
+// Author      : Uchenna Nwanyanwu
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
